4f37424b5fd0f437e998cbb59cb206f7d5b0b974
Neavy18 <genevieve.gt18@gmail.com>
2022-01-23 18:33:40 -0500

    Add API call to new server

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,31 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 
-
-const days = [
+const appointments = [
   {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
   {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
   {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
   },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
 ];
 
 export default function Application(props) {
 
-  const [value, onChange] = useState("Monday");
+  /* INSTRUCTIONS CALLED FOR THESE TO BE PUT AS VALUE AND ONCHANGE BUT IT WAS VERY CONFUSING */
+  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  const appointmentsArr =  appointments.map((appointment) => {
+    
+    return <Appointment key={appointment.id} {...appointment}/>
+  });
   
+ useEffect(() => {
+   const apiDays = "/api/days"
+   axios.get(apiDays).then(response => {
+     setDays(response.data)
+   })
+ }, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -38,8 +74,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
           days={days}
-          value={day}
-          onChange={setDay}
+          day={day}
+          setDays={setDays}
           />
         </nav>
         <img
@@ -49,7 +85,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentsArr}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,12 +4,11 @@ import Header from "./Header";
 import Show from "./Show"
 import Empty from "./Empty";
 
-
 export default function Appointment(props) {
 
   const {time, interview} = props;
 
-  const interviewTruthy = (interview ? <Show name={interview.student} interviewer = {interview.interviewer}/>: <Empty/>)
+  const interviewTruthy = (interview ? <Show student={interview.student} interviewer = {interview.interviewer}/>: <Empty/>)
   
   return (
     <article className="appointment">
